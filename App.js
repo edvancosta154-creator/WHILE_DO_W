1b06283b77fc7aeee59db086e8901e435e9b07b2
Edivan da Silva Costa <edvancosta154@gmail.com>
2026-04-07 16:22:24 -0400

    nao gostei

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 const r = require('readline-sync');
 
+// média com while 
 
-// variaveis
-let soma = 0;
-let contador = 0;
+let somaDoWhile =0;
+let contadorDoWhile = 1;
 
-//contador para controlar o numero de notas
-while (contador <= 5) { 
-    // solicita a nota do usuário
-    let nota = perseFloat(r.question(`Nota ${contador}: `));
-//realizar o calculo das notas, 4 notas.
-soma += nota;
-// incrementa o contador para a proxima nota
-contador++;
-}
-// define a media a soma das notas dividido por 4 obter a média.
-let media = soma / 5;
-// printar a media e usa toFixed para limitar a 2 casas decimais.
-console.log(`A média é: ${media.toFixed(2)}`);
+do {
+    let nota = parseFloat(r.question(`nota ${contadorDoWhile}: `));
+    somaDoWhile += nota;
+    contadorDoWhile++;
+
+} while (contadorDoWhile <= 5);
+
+let mediaDoWhile= somaDoWhile / 5;
+console.log("A média é:  " + mediaDoWhile.toFixed(2));
